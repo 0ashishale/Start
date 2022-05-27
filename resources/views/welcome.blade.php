9f4113e4fb1c4a72bb86d864706839c8b477005d
0ashishale <ashishale42@gmail.com>
2022-05-27 22:34:58 +0545

    first page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,17 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Club</title>
+  
   <link rel="stylesheet" href = "/css/navbar.css" >
-</head>
+  <script src="https://kit.fontawesome.com/77454ad617.js" crossorigin="anonymous"></script>
+  </head>
 <body>
   <!-- -----------Header Section------------ -->
    <section class="header">
       <div class="nav-bar">
         <ul>
           <li><a href="#">Home</a></li>
-          <li><a href="#">About US</a></li>
+          <li><a href="/about">About US</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Gallery</a></li>
           <li><a href="#">Events</a></li>
@@ -32,7 +34,9 @@
     <!-- ---------------Content Section--------------->
 
 
-    <section class="book-table">
+    <section class="book-table"> 
+      
+    <b><a href="#" class="event" class="heading">EVENTS</a> </b><br/>
       <div class="booktable-row">
         <img src="{{ asset('images/table2.jpg') }}" alt="" >
         <h1>NON STOP PARTY</h1>
@@ -63,7 +67,7 @@
     
     <section class="services">
        
-        <b><a href="#"  id="h1">OUR SERVICES</a> </b><br/>
+        <b><a href="#"  class="heading">OUR SERVICES</a> </b><br/>
           <div class="services-row">
                <a href="">CORPORATE EVENTS</a> <br/> <br/>
                 <p>Need a great place to arrange an event <br/> for your business family? You are welcome.</p>
@@ -105,8 +109,8 @@
     <!-- -------------Gallery Section--------- -->
 
       <section class="gallery">
-        <h1>GALLERY</h1>
-
+        <h1 class="heading">GALLERY</h1>
+      <div class="services-row" >
         <img src="{{ asset('images/table2.jpg') }}" alt="" >
         <img src="{{ asset('images/table2.jpg') }}" alt="" >
         <img src="{{ asset('images/table2.jpg') }}" alt="" >
@@ -115,11 +119,30 @@
         <img src="{{ asset('images/table2.jpg') }}" alt="" >
         <img src="{{ asset('images/table2.jpg') }}" alt="" >
         <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        </div>
 
 
       </section>
 
 
-   
+  <section class="footer">
+    <div class="info-bar">
+    <p id="address">Phoolbari-11 <br/> Kaski, Nepal</p>
+    <p id="phone">Phone : 00000000 <br> Fax : 0000000</p>
+    </div>
+    <div class="icons " ></div>
+    <i class="fa-brands fa-facebook"></i>
+    <i class="fa-brands fa-twitter"></i>
+    <i class="fa-brands fa-linkedin"></i>
+    <i class="fa-brands fa-instagram"></i>
+          
+
+   <p id="bholenath">© 2017 All Rights Reserved Terms of Use and Privacy Policy. Design by BHOLENATH.
+
+</p>
+  </section>
+
 </body>
 </html>
