01c16ff036bd4cfde5eb14bddf8355c67a0e165d
0ashishale <ashishale42@gmail.com>
2022-05-27 11:19:10 +0545

    services and gallery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,6 +59,65 @@
 
     </section>
 
+<!-- ---------------Services Section-------------- -->
+    
+    <section class="services">
+       
+        <b><a href="#"  id="h1">OUR SERVICES</a> </b><br/>
+          <div class="services-row">
+               <a href="">CORPORATE EVENTS</a> <br/> <br/>
+                <p>Need a great place to arrange an event <br/> for your business family? You are welcome.</p>
+          </div>
+
+          <div class="services-row">
+          <a href="">BRANDED EVENTS </a> <br/> <br/>
+                <p>We will increase your brand awareness  with<br/>  our special service for big companies.</p>
+          </div>
+
+          <div class="services-row">
+                <a href="">COMMERICIAL SHOOTS</a> <br/> <br/>
+                <p>You will remember your party forever thanks <br/> to our professional photographer.</p>
+          </div>
+
+          <div class="services-row">
+                <a href="">HEN PARTIES</a> <br/> <br/>
+                <p>Feel the magic of dance floor with your <br/> friends before it is not too late.</p>
+          </div>
+
+          <div class="services-row">
+               <a href=""> BIRTHDAY PARTIES </a> <br/> <br/>
+                <p>Happy birthday! Celebrate in our club <br/> with special discounts!
+
+</p>
+          </div>
+
+          <div class="services-row">
+                <a href="#">VIP SERVICES</a> <br/> <br/>
+                <p>Premium services for special guests. <br/> Available only for loyal clients.</p>
+          </div>
+          
+          
+    </section>
+
+
+
+
+    <!-- -------------Gallery Section--------- -->
+
+      <section class="gallery">
+        <h1>GALLERY</h1>
+
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+        <img src="{{ asset('images/table2.jpg') }}" alt="" >
+
+
+      </section>
 
 
    
