974dfb97765ab7329c6905f0057b91ae3b981e06
alina232 <timilsina.alina12@gmail.com>
2022-05-30 14:06:46 +0545

    header for pages added

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -13,22 +13,22 @@ class RouteController extends Controller
     }
 
     function services(){
-        return view('services', ['text'=>'write something', 'title'=>'Our Services']);
+        return view('services', ['text'=>'Full services to meet your every need', 'title'=>'Our Services']);
     }
     function reservation(){
         return view('reservation',['text'=>'Reserve Your Table', 'title'=>'Reservation']);
     }
 
     function events(){
-        return view('events');
+        return view('events',['text'=>'See The Events Happening Now', 'title'=>'Events']);
     }
     function gallery(){
-        return view('gallery');
+        return view('gallery',['text'=>'Memories from the Events', 'title'=>'Gallery']);
     }
     function about(){
-        return view('about');
+        return view('about',['text'=>'Know More About Us', 'title'=>'About Us']);
     }
     function contact(){
-        return view('contact');
+        return view('contact',['text'=>'Contact for more details', 'title'=>'Reservation']);
     }
 }
