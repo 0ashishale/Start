3f9f75bc516ea012a2a3e2bed27a808344aa3925
RAJESHSHREES <rajeshshrees0829@gmail.com>
2022-06-02 22:33:58 +0545

    reservation all changes

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -4,50 +4,36 @@
 
 
 <section class="reservation">
-  <div class="title"> <h1>WANT TO BOOK A TABLE FOR A SPECIAL OCCASION ?</h1><br>
+ <!-- <div class="title"> <h1>WANT TO BOOK A TABLE FOR A SPECIAL OCCASION ?</h1><br>
      <p>Grinnesso is multi-purpose venue. Make an enquiry about hiring our space.</p>
-</div>
-  <br>
-  <FORM action="#">
-    <div class="input-box">
-         <span class="details">Name:</span><br>
-         
-         <input type="text" class="latter" placeholder="Enter your name"required>
+</div>-->
+  <form>
+       <div class="input-group">
+         <input type="text" id="Name"required>
+         <label for="name">your Name</label>
        </div>
-       <br>
-       <div class="input-box">
-         <span class="details">E-mail:</span><br>
-         <input type="text" class="latter" placeholder="Enter your email"required>
+       <div class="input-group">
+         <input type="email" id="email"required>
+         <label for="email">Email Id</label>
        </div>
-       <br>
-       <div class="input-box">
-         <span class="details">Phone No:</span><br>
-         <input type="number" class="latter"  placeholder="Enter your phone number"required>
-         
-       </div><br>
-       <div class="input-box">
-         <span class="details">No. of people:</span><br>
-         <input type="number" class="latter"  placeholder="Enter No. of poeple"required>
-         
+       <div class="input-group">
+         <input type="text" id="number"required>
+         <label for="number">Phone No.</label>
        </div>
-       <br>
-       <div class="input-box">
-         <span class="details">Message:</span><br>
-         <input type="text" class="message" placeholder="Enter your message"required>
+       <div class="input-group">
+         <textarea id="message" rows="8"></textarea>
+         <label for="message">your Message</label>
        </div>
-       <br>
-       <div class="input-box">
+       <div>
+          <button type="submit">SUBMIT</button>
+       </div>
+       <div class="input-group">
          <span class="details">Select Branches:</span><br>
          <input type="radio" name="branch">  <h3>Pokhara</h3> 
          <input type="radio" name="branch"><h3>Shyanja</h3> 
          <input type="radio" name="branch"><h3>Baglung</h3> <br><br>
-       <div class="button">
-         <input type="submit" class="value" value="Send message">
-       </div> <br>
-      
-
        </div>
-  </FORM>
+      </form>
 </section>
 
 @endsection()
