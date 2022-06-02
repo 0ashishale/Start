d3b888f7d739511634d87ca854495530e9f824b5
RAJESHSHREES <rajeshshrees0829@gmail.com>
2022-06-02 16:51:55 +0545

    reservation changes

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -36,9 +36,17 @@
          <input type="text" class="message" placeholder="Enter your message"required>
        </div>
        <br>
+       <div class="input-box">
+         <span class="details">Select Branches:</span><br>
+         <input type="radio" name="branch">  <h3>Pokhara</h3> 
+         <input type="radio" name="branch"><h3>Shyanja</h3> 
+         <input type="radio" name="branch"><h3>Baglung</h3> <br><br>
        <div class="button">
          <input type="submit" class="value" value="Send message">
        </div> <br>
+      
+
+       </div>
   </FORM>
 </section>
 
