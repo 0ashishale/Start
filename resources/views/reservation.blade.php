130d6e7cabf3ad0ba3dabbe120655358f17e4997
RAJESHSHREES <rajeshshrees0829@gmail.com>
2022-06-02 23:20:51 +0545

    change in reseervation tabel

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -8,31 +8,33 @@
      <p>Grinnesso is multi-purpose venue. Make an enquiry about hiring our space.</p>
 </div>-->
   <form>
+    <div class="row">
        <div class="input-group">
          <input type="text" id="Name"required>
          <label for="name">your Name</label>
        </div>
        <div class="input-group">
+         <input type="text" id="number"required>
+         <label for="number">Phone No.</label>
+       </div>
+    </div>
+       <div class="input-group">
          <input type="email" id="email"required>
          <label for="email">Email Id</label>
        </div>
        <div class="input-group">
-         <input type="text" id="number"required>
-         <label for="number">Phone No.</label>
-       </div>
-       <div class="input-group">
-         <textarea id="message" rows="8"></textarea>
+         <textarea id="message" rows="8" required></textarea>
          <label for="message">your Message</label>
        </div>
        <div>
           <button type="submit">SUBMIT</button>
        </div>
-       <div class="input-group">
+       <!--<div class="input-group">
          <span class="details">Select Branches:</span><br>
          <input type="radio" name="branch">  <h3>Pokhara</h3> 
          <input type="radio" name="branch"><h3>Shyanja</h3> 
          <input type="radio" name="branch"><h3>Baglung</h3> <br><br>
-       </div>
+       </div>-->
       </form>
 </section>
 
