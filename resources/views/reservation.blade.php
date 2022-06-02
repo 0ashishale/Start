58cb1bae8c398f236ba97247691f77da8fcbcca7
RAJESHSHREES <rajeshshrees0829@gmail.com>
2022-06-03 01:33:35 +0545

    final change in reservation

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -2,40 +2,49 @@
 
 @section('event')
 
+<section>
+<div class="reservation">
+         <form>
+           <div class="row1">
+              <div class="input-group">
+                <input type="text" id="Name"required>
+                <label for="name">your Name</label>
+              </div>
+              <div class="input-group">
+                <input type="text" id="number"required>
+                <label for="number">Phone No.</label>
+              </div>
+           </div>
+           <div class="row2">
+             <div class="input-group">
+                <input type="number" id="people"required>
+                <label for="number">NO. Of People</label>
+              </div>
+              <div class="input-group">
+                <input type="date" name="date" required>
+                <label for="date"></label>
+              </div>
+           </div>
+              <div class="input-group">
+                <input type="email" id="email"required>
+                <label for="email">Email Id</label>
+              </div>
 
-<section class="reservation">
- <!-- <div class="title"> <h1>WANT TO BOOK A TABLE FOR A SPECIAL OCCASION ?</h1><br>
-     <p>Grinnesso is multi-purpose venue. Make an enquiry about hiring our space.</p>
-</div>-->
-  <form>
-    <div class="row">
-       <div class="input-group">
-         <input type="text" id="Name"required>
-         <label for="name">your Name</label>
-       </div>
-       <div class="input-group">
-         <input type="text" id="number"required>
-         <label for="number">Phone No.</label>
-       </div>
-    </div>
-       <div class="input-group">
-         <input type="email" id="email"required>
-         <label for="email">Email Id</label>
-       </div>
-       <div class="input-group">
-         <textarea id="message" rows="8" required></textarea>
-         <label for="message">your Message</label>
-       </div>
-       <div>
-          <button type="submit">SUBMIT</button>
-       </div>
-       <!--<div class="input-group">
-         <span class="details">Select Branches:</span><br>
-         <input type="radio" name="branch">  <h3>Pokhara</h3> 
-         <input type="radio" name="branch"><h3>Shyanja</h3> 
-         <input type="radio" name="branch"><h3>Baglung</h3> <br><br>
-       </div>-->
-      </form>
-</section>
+              <div class="input-group">
+                <textarea id="message" rows="8" required></textarea>
+                <label for="message">Your Message</label>
+              </div>
+              <div>
+                 <button type="submit">SUBMIT</button>
+              </div>
+              <!--<div class="input-group">
+                <span class="details">Select Branches:</span><br>
+                <input type="radio" name="branch">  <h3>Pokhara</h3> 
+                <input type="radio" name="branch"><h3>Shyanja</h3> 
+                <input type="radio" name="branch"><h3>Baglung</h3> <br><br>
+              </div>-->
+          </form>
+        </div>
+</section>        
 
 @endsection()
