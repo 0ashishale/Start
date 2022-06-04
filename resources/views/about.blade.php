9890579b79cc59288ee738c7799cd5780bd495f0
0ashishale <ashishale42@gmail.com>
2022-06-04 22:19:28 +0545

    about page added

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,63 +4,41 @@
 @section('event')
 
 <section class="about-us">
-       
-       <b  class="head" >About Us</b> <br>
-         <div class="about-text">
-           <b>Opening Hours: 9PM-3AM</b>
-           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Sequi reiciendis ipsam velit harum reprehenderit impedit quidem 
-             natus placeat, suscipit odio pariatur neque at hic modi dolore 
-             veritatis autem voluptatibus cum.</p>
-            
-         </div>
+  <div class="container">
+    <div class="content-section">
+      <div class="title">
+        <h1>About Us</h1>
+      </div>
 
-       
-             <span class="contact">
-                  <i class="fa-solid fa-phone"></i>
-                  <h1>+9770000000000</h1>
-                  <a href="/reservation"><i class="fa fa-paper-plane"></i>Reservation</a>
-            </span>
+      <div class="content">
+        <h4>Opening Hours: 9PM-3AM</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nisi, atque sequi totam voluptas veritatis expedita dolorum praesentium ratione,
+          accusamus id ad fugit, deserunt accusantium incidunt nam delectus labore qui perferendis!</p>
+        <a href="{{url('/reservation')}}"><button>Reservation</button></a>
+      </div>
+      <div class="image-section">
+        <img src="{{asset('images/logo.png')}}" alt="">
+      </div>
 
-       
-       
-         
-         
+    </div>
+    <div class="social content">
+      <a href="#"><i class="fa-solid fa-phone"> +9779876543218</i></a>
+      <a href="#"><i class="fa-brands fa-facebook-f"> Facebook</i></a>
+      <a href="#"><i class="fa-brands fa-instagram"> Instagram</i></a>
+    </div>
+
+  </div>
+
+  <div class="map">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7029.990810664071!2d83.98180702529555!3d28.237819136874638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3995945d57d28ba5%3A0xdd52869218279546!2sShree%20Bindhyabasini%20Temple!5e0!3m2!1sen!2snp!4v1653900842774!5m2!1sen!2snp"
+      width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+  </div>
+
+
 </section>
-
-
-
-
-
-    <section>
-<div class="reservation">
-         <form>
-              <div class="input-group">
-                <input type="text" id="Name"required>
-                <label for="name"><i class="fa fa-user"></i>your Name</label>
-              </div>
-              <div class="input-group">
-                <input type="email" id="email"required>
-                <label for="email"><i class="fa fa-envelope" ></i>Email Id</label>
-              </div>
-
-              <div class="input-group">
-                <textarea id="message" rows="8" required></textarea>
-                <label for="message"><i class="fa fa-comments-o"></i>Your Message</label>
-              </div>
-              <div>
-                 <button type="submit">SEND<i class="fa fa-paper-plane"></i></button>
-              </div>
-              <!--<div class="input-group">
-                <span class="details">Select Branches:</span><br>
-                <input type="radio" name="branch">  <h3>Pokhara</h3> 
-                <input type="radio" name="branch"><h3>Shyanja</h3> 
-                <input type="radio" name="branch"><h3>Baglung</h3> <br><br>
-              </div>-->
-          </form>
-        </div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7029.990810664071!2d83.98180702529555!3d28.237819136874638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3995945d57d28ba5%3A0xdd52869218279546!2sShree%20Bindhyabasini%20Temple!5e0!3m2!1sen!2snp!4v1653900842774!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-</section>        
 
 @endsection()
