57aa45d04bd709faf81253b28e96e17736585c9d
RAJESHSHREES <rajeshshrees0829@gmail.com>
2022-06-08 15:01:29 +0545

    changes in gallery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,27 +82,42 @@
 
       <section class="gallery">
         <h1 class="heading">GALLERY</h1>
-      <div class="services-row" >
-        <img src="{{ asset('images/a.jpg') }}" alt="" >
-        <img src="{{ asset('images/b.jpg') }}" alt="" >
-        <img src="{{ asset('images/c.jpg') }}" alt="" >
-        <img src="{{ asset('images/reservation.jpg') }}" alt="" >
-        <img src="{{ asset('images/d.jpg') }}" alt="" >
-        <img src="{{ asset('images/e.jpg') }}" alt="" >
-        <img src="{{ asset('images/b.jpg') }}" alt="" >
-        <img src="{{ asset('images/img.jpg') }}" alt="" >
-        <img src="{{ asset('images/reservation.jpg') }}" alt="" >
-        <img src="{{ asset('images/table2.jpg') }}" alt="" >
-        <img src="{{ asset('images/a.jpg') }}" alt="" >
-        <img src="{{ asset('images/e.jpg') }}" alt="" >
+        <div class="photos">
+         <div class="container">
+           <a href="{{ asset('images/a.jpg') }}"data-lightbox="models" data-title="caption1">
+           <img src="{{ asset('images/a.jpg') }}" alt="">
+          </a>
+          <a href="images/b.jpg"data-lightbox="models" data-title="caption2">
+            <img src="images/b.jpg" alt="">
+           </a>
+           <a href="images/c.jpg"data-lightbox="models" data-title="caption3">
+            <img src="images/c.jpg" alt="">
+           </a>
+           <a href="images/d.jpg"data-lightbox="models" data-title="caption4">
+            <img src="images/d.jpg" alt="">
+           </a>
+           <a href="images/e.jpg"data-lightbox="models" data-title="caption5">
+            <img src="images/e.jpg" alt="">
+           </a>
+           <a href="images/img.jpg"data-lightbox="models" data-title="caption6">
+            <img src="images/img.jpg" alt="">
+           </a>
+           <a href="images/reservation.jpg"data-lightbox="models" data-title="caption7">
+            <img src="images/reservation.jpg" alt="">
+           </a>
+           <a href="images/table1.jpg"data-lightbox="models" data-title="caption8">
+            <img src="images/table1.jpg" alt="">
+           </a>
+           <a href="images/background.jpg"data-lightbox="models" data-title="caption9">
+            <img src="images/background.jpg" alt="">
+           </a>
         </div>
+         
+       </div>
 <!---reservation table-->
   <section>
-  <div>
-      <b><a href="#"  class="heading">RESERVATION TABLE</TABLE></a> </b>
-      </div>
-      <br>
       <div class="reservation">
+      <a href="#"  class="heading">RESERVATION TABLE</TABLE></a> 
          <form>
            <div class="row1">
               <div class="input-group">
